bb9cfac9d19789acef22726a6495a308b628ac64
Armand Kamffer <armand.kamffer@entelect.co.za>
2021-06-25 12:37:34 +0200

    List session participants

---
+++ src/app/models/planning-session-state.enum.ts
@@ -0,0 +1,7 @@
+export enum PlanningSessionState {
+    none,
+    voting,
+    finishedVoting,
+    error,
+    sessionEnded
+}
--- src/app/planning/planning-host-landing/planning-host-landing.component.ts
+++ src/app/planning/planning-host-landing/planning-host-landing.component.ts
@@ -5,6 +5,11 @@ import { UUID } from 'angular2-uuid';
 import { PlanningCommandHostSend } from 'src/app/models/host/planning-command-host-send';
 import { PlanningHostCommandMapper } from 'src/app/mapper/planning-host-command-mapper';
 import { PlanningCard } from 'src/app/models/planning-card.enum';
+import { PlanningCommandHostReceive } from 'src/app/models/host/planning-command-host-receive';
+import { PlanningCommandHostReceiveType } from 'src/app/models/host/planning-command-host-receive-type.enum';
+import { PlanningSessionState } from 'src/app/models/planning-session-state.enum';
+import { PlanningSessionStateMessage } from 'src/app/models/messages/planning-session-state-message';
+import { PlanningParticipant } from 'src/app/models/planning-participant';
 
 @Component({
   selector: 'app-planning-host-landing',
@@ -16,7 +21,7 @@ export class PlanningHostLandingComponent implements OnInit {
 
   webSocketSubject = webSocket({
     binaryType: 'arraybuffer',
-    url: 'ws://localhost:8080/planning/host', 
+    url: 'ws://localhost:8080/planning/host',
     serializer: (msg: Uint8Array) => {
       const offset = msg.byteOffset;
       const length = msg.byteLength;
@@ -25,24 +30,48 @@ export class PlanningHostLandingComponent implements OnInit {
     deserializer: msg => new Uint8Array(msg.data as ArrayBuffer)
   })
   subscription: Subscription
-  
+
   uuid = UUID.UUID()
   @Input() sessionName: string = ""
   @Input() userName: string = ""
   @Input() availableCards: PlanningCard[] = []
-  
+
   ticketTitle: string = ""
   ticketDescription: string = ""
+
+  state = PlanningSessionState.none
+  sessionCode: string = ""
+  participants: PlanningParticipant[] = []
+
+  get isNoneState() {
+    return this.state == PlanningSessionState.none
+  }
+
+  get isVotingState() {
+    return this.state == PlanningSessionState.voting
+  }
+
+  get isFinishedVotingState() {
+    return this.state == PlanningSessionState.finishedVoting
+  }
+
+  get isErrorState() {
+    return this.state == PlanningSessionState.error
+  }
+
+  get isSessionEndedState() {
+    return this.state == PlanningSessionState.sessionEnded
+  }
 
   constructor() {
     this.subscription = this.webSocketSubject.subscribe(
       msg => {
         let jsonObject = JSON.parse(new TextDecoder().decode(msg))
         let incomingCommand = this.hostCommandMapper.mapIncomingCommand(jsonObject)
-        console.log('message received: ', jsonObject, incomingCommand)
+        this.execute(incomingCommand)
       },
       err => console.log(err),
-      () => console.log('complete')
+      () => this.state = PlanningSessionState.sessionEnded
     )
   }
 
@@ -60,5 +89,36 @@ export class PlanningHostLandingComponent implements OnInit {
   onClickAddTicket() {
     var command = this.hostCommandMapper.mapAddTicketCommand(this.uuid, this.ticketTitle, this.ticketDescription)
     this.sendCommand(command)
+  }
+
+  onClickFinishVoting() {
+    var command = this.hostCommandMapper.mapFinishVotingCommand(this.uuid)
+    this.sendCommand(command)
+  }
+
+  onClickEndSession() {
+    var command = this.hostCommandMapper.mapEndSessionCommand(this.uuid)
+    this.sendCommand(command)
+  }
+
+  execute(command: PlanningCommandHostReceive) {
+    console.log('message received: ', command)
+    switch (command.type) {
+      case PlanningCommandHostReceiveType.noneState:
+        this.state = PlanningSessionState.none
+        var stateMessage = command.message as PlanningSessionStateMessage
+        this.sessionCode = stateMessage.sessionCode
+        this.participants = stateMessage.participants
+        break
+      case PlanningCommandHostReceiveType.votingState:
+        this.state = PlanningSessionState.voting
+        break
+      case PlanningCommandHostReceiveType.finishedState:
+        this.state = PlanningSessionState.finishedVoting
+        break
+      case PlanningCommandHostReceiveType.invalidCommand:
+        this.state = PlanningSessionState.error
+        break
+    }
   }
 }
