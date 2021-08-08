c4608b742f21a496699547469b2a1ac1fc0539a0
Armand Kamffer <akamffer@mweb.co.za>
2021-08-08 19:32:52 +0200

    Add displaying user connections status

--- src/app/models/planning-participant.ts
+++ src/app/models/planning-participant.ts
@@ -1,10 +1,12 @@
 export class PlanningParticipant {
     participantId: string
     name: string
+    connected: boolean
     highlighted: boolean = false
     
-    constructor(participantId: string, name: string) {
+    constructor(participantId: string, name: string, connected: boolean) {
         this.participantId = participantId
         this.name = name
+        this.connected = connected
     }
 }
